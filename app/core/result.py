5ecc300e1570503205f8369bb412e1180e589ad1
Mario Sánchez <mariosb2004@gmail.com>
2026-03-10 13:16:48 +0100

    feat: added result type

---
+++ app/core/result.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass(frozen=True)
+class Ok[T]:
+    """Represents a succesful outcome of an operation.
+
+    Attributes
+    ----------
+    value: `T`
+        The successful result data.
+    """
+
+    value: T
+
+    def is_ok(self) -> bool:
+        """Returns True if the result is Ok."""
+        return True
+
+    def is_err(self) -> bool:
+        """Returns True if the result is Err."""
+        return False
+
+    def unwrap(self) -> T:
+        """Returns the contained value if the result is Ok, otherwise raises an exception."""
+        return self.value
+
+
+@dataclass(frozen=True)
+class Err[E]:
+    """Represents a failed outcome of an operation.
+
+    Attributes
+    ----------
+    error: `E`
+        The error message or data describing the failure.
+    """
+
+    error: E
+
+    def is_ok(self) -> bool:
+        """Returns True if the result is Ok."""
+        return False
+
+    def is_err(self) -> bool:
+        """Returns True if the result is Err."""
+        return True
+
+    def unwrap(self) -> Any:
+        """Raises an exception with the contained error message."""
+        raise RuntimeError(f"Called unwrap on an Err value: {self.error}")
+
+
+# A type alias for a result that can be either Ok or Err.
+type Result[T, E] = Ok[T] | Err[E]
