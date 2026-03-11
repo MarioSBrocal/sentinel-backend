bd71936cd3a0bb08764db4a29a23b68bef1e1422
Mario Sánchez <mariosb2004@gmail.com>
2026-03-11 22:43:43 +0100

    feat: added auth errors and finished result

--- app/core/errors.py
+++ app/core/errors.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+
 class AppError(Exception):
     """Base class for all controllable errors in the application.
     It inherits from Exception, so it can be `raised` if necessary.
@@ -7,3 +10,39 @@ class AppError(Exception):
     def message(self) -> str:
         """Returns a human-readable error message."""
         return "An unexpected error occurred in the application."
+
+
+@dataclass
+class DatabaseError(AppError):
+    detail: str
+
+
+@dataclass
+class UserAlreadyExistsError(AppError):
+    """Raised when a user with a given email already exists."""
+
+    email: str
+
+    @property
+    def message(self) -> str:
+        return f"User with email {self.email} already exists."
+
+
+@dataclass
+class UserNotFoundError(AppError):
+    """Raised when a user with a given email is not found."""
+
+    email: str
+
+    @property
+    def message(self) -> str:
+        return f"User with email {self.email} not found."
+
+
+@dataclass
+class InvalidCredentialsError(AppError):
+    """Raised when authentication fails due to invalid credentials."""
+
+    @property
+    def message(self) -> str:
+        return "Invalid email or password."
